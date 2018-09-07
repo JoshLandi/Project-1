9a78fc046b7aa64162c9f7e7cd80042505b01a4c
Josh L <joshtlandi@gmail.com>
2018-09-07 12:27:44 -0700

    Making it look/feel better

--- Javascript/script.js
+++ Javascript/script.js
@@ -14,26 +14,23 @@ $.ajax({
     console.log(wQueryURL);
     console.log(response);
 
-//Showing City Chosen
-$("#city").html(currentCity);
+//Getting Weather Icon
+var wIconCode = response.weather[0].icon;
+console.log(wIconCode);
+var iconUrl = "http://openweathermap.org/img/w/" + wIconCode + ".png";
+
+//Showing City Chosen and Weather Icon
+$("#city").html(currentCity + "<img src='" + iconUrl  + "' alt='Icon Showing Current Weather'>");
     
 //Showing temp
     var currentTemp = response.main.temp;
-    var showResults1 = $("<p>");
-    showResults1.append(currentTemp + " degrees");
-    $("#tempH").append(showResults1);
+    var showResults1 = $("#tempH");
+    showResults1.append(" " + currentTemp + " degrees");
 
 //Showing Humidity
     var currentHumidity = response.main.humidity;
-    var showResults2 = $("<p>");
-    showResults2.append(currentHumidity + " %");
-    $("#humidityH").append(showResults2);
+    var showResults2 = $("#humidityH");
+    showResults2.append(" " + currentHumidity + " %");
 
-//Getting Weather Icon
-    var wIconCode = response.weather[0].icon;
-    console.log(wIconCode);
-    var iconUrl = "http://openweathermap.org/img/w/" + wIconCode + ".png";
-//Displaying weather icon
-    $(".icon").html("<img src='" + iconUrl  + "' alt='Icon Showing Current Weather'>");
 
 });
