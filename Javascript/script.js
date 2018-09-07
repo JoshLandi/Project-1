36e4ea8a598625aece4b871b699375a8b19af738
Josh L <joshtlandi@gmail.com>
2018-09-07 12:08:36 -0700

    added temp, humidity, weather icon

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,22 +1,37 @@
-//Variables for OWM AJAX call
+//Variables for OWM AJAX call (converted Kelvin to Imperial units)
 var currentCity = "Tucson";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + 
-                "&APPID=a13dc362eeacbccc63e8cbc432ab2eb5";
+var wQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + 
+                "&units=imperial&APPID=a13dc362eeacbccc63e8cbc432ab2eb5";
 
 //AJAX call to OpenWeatherMap
 $.ajax({
-    url: queryURL,
+    url: wQueryURL,
     method: "GET"
   })
 
   .then(function(response) {
 
-    console.log(queryURL);
+    console.log(wQueryURL);
     console.log(response);
+
+//Getting Weather Icon
+var wIconCode = response.weather[0].icon;
+console.log(wIconCode);
+var iconUrl = "http://openweathermap.org/img/w/" + wIconCode + ".png";
     
-//Working up to this point so far
-    var results = response.data;
-    var showResults = $("<p>");
-    showResults.append(results);
-    $("#weatherChartDiv").append(showResults);
+//Showing temp
+    var currentTemp = response.main.temp;
+    var showResults1 = $("<p>");
+    showResults1.append(currentTemp + " degrees");
+    $("#tempH").append(showResults1);
+
+//Showing Humidity
+    var currentHumidity = response.main.humidity;
+    var showResults2 = $("<p>");
+    showResults2.append(currentHumidity + "%");
+    $("#humidityH").append(showResults2);
+
+//Displaying weather icon
+$(".icon").html("<img src='" + iconUrl  + "' alt='Icon Showing Current Weather'>");
+
 });
