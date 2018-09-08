4477871c6437baab789dd4bb28e3d06a7d077293
Josh L <joshtlandi@gmail.com>
2018-09-08 10:09:27 -0700

    Other stuff

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,5 +1,24 @@
-//Variables for OWM AJAX call (converted Kelvin to Imperial units)
 var currentCity = "Tucson";
+//Function for looking up a different city (not sure we will use this)
+//function cityLookUp () {
+   // var lookUpCity = $("#city-input").val().trim();
+   // function modifyCurrentCity (c) {
+       // c = lookUpCity;
+   // }
+   // modifyCurrentCity(currentCity)
+    //console.log(currentCity);
+
+//}
+
+//Connecting HTML Look Up button to JS function
+//$("#add-city").on("click", function(event){
+
+    //cityLookUp();
+    //console.log(currentCity);
+//});
+
+
+//Variables for OWM AJAX call (converted Kelvin to Imperial units)
 var wQueryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + currentCity + 
                 "&units=imperial&APPID=a13dc362eeacbccc63e8cbc432ab2eb5";
 
